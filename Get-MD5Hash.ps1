f34793d38b3477a074620103331264e3b3395576
glaisne <glaisne@gmail.com>
2016-09-19 12:01:56 -0400

    Fixed an issue with path not being resolved when '.\...' or a PSDrive path.

--- Get-MD5Hash.ps1
+++ Get-MD5Hash.ps1
@@ -7,6 +7,7 @@
     # This Get-MD5 function sourced from:
     # http://blogs.msdn.com/powershell/archive/2006/04/25/583225.aspx
     $HashAlgorithm = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    $Path = (resolve-path $Path).ProviderPath
     $Stream = [System.IO.File]::OpenRead($Path)
     try {
         $HashByteArray = $HashAlgorithm.ComputeHash($Stream)
